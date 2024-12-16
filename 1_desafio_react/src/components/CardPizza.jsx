b2910b2f060860ae1240fce7f4371e0cfe5ea50b
Francisco Muñoz <munozjaraf@gmail.com>
2024-12-15 23:21:30 -0300

    hito 1 - faltan detalles de estilo

---
+++ 1_desafio_react/src/components/CardPizza.jsx
@@ -0,0 +1,29 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const CardPizza = ({ img, name, ingredients, price }) => {
+  return (
+    <>
+      <Card style={{ width: "20rem" }}>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>Pizza {name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted text-center">
+            Ingredientes
+          </Card.Subtitle>
+          <Card.Text>🍕{ingredients.join(", ")}</Card.Text>
+          <Card.Title className="text-center">Precio ${price}</Card.Title>
+          <div className="d-flex justify-content-between mt-3">
+            <Button variant="outline-dark">ver más 👀</Button>
+            <Button variant="dark"> 🛒 Añadir</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default CardPizza;
+
+
+// intento con boostrap react (casi me mori)
---
+++ 1_desafio_react/src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Navbar = () => {
+  const total = 25000;
+  const token = false;
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <h2 className="navbar-brand">Pizzería Mammá Mia</h2>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto gap-3">
+            {/* home */}
+            <li className="nav-item">
+              <button className="btn btn-outline-light">🍕 Home</button>
+            </li>
+
+            {/* token */}
+
+            {token ? (
+              <>
+                <li className="nav-item">
+                  <button className="btn btn-outline-light">🔓 Profile</button>
+                </li>
+                <li className="nav-item">
+                  <button className="btn btn-outline-light">🔒 Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <button className="btn btn-outline-light">🔐 Login</button>
+                </li>
+                <li className="nav-item">
+                  <button className="btn btn-outline-light">🔐 Register</button>
+                </li>
+              </>
+            )}
+          </ul>
+          {/* total */}
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <button className="btn btn-outline-primary nav-link">
+                🛒 Total: $ {total}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
