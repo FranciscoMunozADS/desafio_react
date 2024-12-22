0775f26ddc17f3e978a31789cb687c5e24e7f8da
Francisco Muñoz <munozjaraf@gmail.com>
2024-12-22 00:07:47 -0300

    hito 2 terminado

--- 1_desafio_react/src/components/Login.jsx
+++ 1_desafio_react/src/components/Login.jsx
@@ -1,36 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [signinData, setSigninData] = useState({
+    email: "",
+    password: "",
+  });
+
+  /* Función para onChange */
+  const updateSigninData = (e) => {
+    const { name, value } = e.target;
+    setSigninData({
+      ...signinData,
+      [name]: value,
+    });
+  };
+
+  /* botón (submit) */
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validaciones dentro del botón
+
+    if (!signinData.email || !signinData.password) {
+      alert("Completa todos los campos.");
+      return;
+    }
+    if (signinData.password.length < 6) {
+      alert("La contraseña debe tener al menos 6 caracteres.");
+      return;
+    }
+
+    alert("Datos correctos.");
+    console.log("Datos correctos:", signinData);
+    setSigninData({
+      email: "",
+      password: "",
+    });
+  };
   return (
     <>
-      <form className="my-5 border border-dark p-4">
-        <h3 className="mb-4">Ingresa</h3>
+      <form
+        className="m-5 border border-dark p-4 shadow-lg rounded bg-light"
+        onSubmit={handleSubmit}
+      >
+        <h3 className="text-center text-primary mb-4">Iniciar sesión</h3>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+          <label htmlFor="exampleInputEmail2" className="form-label">
             Dirección de Email
           </label>
           <input
             type="email"
+            name="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="exampleInputEmail2"
             aria-describedby="emailHelp"
             autoComplete="username"
+            value={signinData.email}
+            onChange={updateSigninData}
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+          <label htmlFor="exampleInputPassword2" className="form-label">
             Contraseña
           </label>
           <input
             type="password"
+            name="password"
             className="form-control"
-            id="exampleInputPassword1"
+            id="exampleInputPassword2"
             autoComplete="off"
+            value={signinData.password}
+            onChange={updateSigninData}
           />
         </div>
 
         <div className="mb-3 form-check"></div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary btn-lg w-25">
           Ingresar
         </button>
       </form>
--- 1_desafio_react/src/components/Register.jsx
+++ 1_desafio_react/src/components/Register.jsx
@@ -1,20 +1,75 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 const Register = () => {
+  const [signupData, setSignupData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  /* Función genérica para onChange */
+
+  const updateSignupData = (e) => {
+    const { name, value } = e.target;
+    setSignupData({
+      ...signupData,
+      [name]: value,
+    });
+  };
+
+  /* Botón (submit) */
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // de esta manera no se actualiza la página (default de submit)
+
+    // Validaciones dentro del botón
+
+    if (
+      !signupData.email ||
+      !signupData.password ||
+      !signupData.confirmPassword
+    ) {
+      alert("Completa todos los campos.");
+      return;
+    }
+    if (signupData.password.length < 6) {
+      alert("La constraseña debe tener al menos 6 caracteres.");
+      return;
+    }
+    if (signupData.password !== signupData.confirmPassword) {
+      alert("Las contraseñas no coinciden.");
+      return;
+    }
+
+    alert("Datos enviados correctamente.");
+    console.log("Datos enviados correctamente:", signupData);
+    setSignupData({
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
   return (
     <>
-      <form className="my-5 border border-dark p-4">
-        <h3 className="mb-4">Registrate</h3>
+      <form
+        className="m-5 border border-dark p-4 shadow-lg rounded bg-light"
+        onSubmit={handleSubmit}
+      >
+        <h3 className="text-center text-primary mb-4">Registrate</h3>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Dirección de Email
           </label>
           <input
             type="email"
+            name="email"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             autoComplete="username"
+            value={signupData.email}
+            onChange={updateSignupData}
           />
           <div id="emailHelp" className="form-text">
             No compartiremos tu email con nadie.
@@ -26,9 +81,12 @@ const Register = () => {
           </label>
           <input
             type="password"
+            name="password"
             className="form-control"
             id="exampleInputPassword1"
             autoComplete="new-password"
+            value={signupData.password}
+            onChange={updateSignupData}
           />
         </div>
         <div className="mb-3">
@@ -37,14 +95,17 @@ const Register = () => {
           </label>
           <input
             type="password"
+            name="confirmPassword"
             className="form-control"
             id="confirmPassword"
             autoComplete="confirm-password"
+            value={signupData.confirmPassword}
+            onChange={updateSignupData}
           />
         </div>
         <div className="mb-3 form-check"></div>
-        <button type="submit" className="btn btn-primary">
-          Registrar
+        <button type="submit" className="btn btn-primary btn-lg w-25">
+          Crear cuenta
         </button>
       </form>
     </>
